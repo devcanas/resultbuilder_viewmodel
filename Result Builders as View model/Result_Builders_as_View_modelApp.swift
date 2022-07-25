1a0cf57e4acb8b05462ccaf488123649e57b0e72
devcanas <devcanas@gmail.com>
2022-07-25 22:55:25 +0100

    Initial Commit

---
+++ Result Builders as View model/Result_Builders_as_View_modelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Result_Builders_as_View_modelApp.swift
+//  Result Builders as View model
+//
+//  Created by Vicente Canas on 25/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Result_Builders_as_View_modelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
